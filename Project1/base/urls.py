f5d380d5127403192a6af81bcbb4d4baa1db825b
govindlenka77 <83697782+govindlenka77@users.noreply.github.com>
2024-10-12 06:43:44 +0530

    Home page code

--- Project1/base/urls.py
+++ Project1/base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('hello/',say_hello,name='say_hello'),
+    path('',home,name='home'),
+    path('say_hello/',say_hello,name='say_hello'),
     path('say_bye/',say_bye, name='say_bye'),
 ]
--- Project1/base/views.py
+++ Project1/base/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Home page")
+
 
 def say_hello(request):
     return HttpResponse("Hello world")
